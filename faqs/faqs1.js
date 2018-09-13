3811ecc4802cba4a3e6f5ad6bca4b8f293eb302f
mrneo0nline <39259021+mrneo0nline@users.noreply.github.com>
2018-09-13 20:54:53 +0700

    just like

--- faqs/faqs1.js
+++ faqs/faqs1.js
@@ -1,32 +1,37 @@
 "use strict";
-var $ = function(id) { return document.getElementById(id); };
+// var $ = function(id) { return document.getElementById(id); };
 
 // the event handler for the click event of each h2 element
 var toggle = function() {
+    // console.log(this);
     var link = this;                    // clicked h2 tag     
     var h2 = link.parentNode;
-    var div = h2.nextElementSibling;  // h2 tag's sibling div tag
+    var div = h2.next();  // h2 tag's sibling div tag
 
     // toggle plus and minus image in h2 elements by adding or removing a class
     if (h2.hasAttribute("class")) { 
-        h2.className = "";	
+        h2.removeAttribute("class");	
     } else { 
         h2.className = "minus";
     }
 
     // toggle div visibility by adding or removing a class
     if (div.hasAttribute("class")) { 
-        div.className = "";
+        div.removeAttribute("class");
     } else { 
         div.className = "open"; 
     } 
 };
 
 window.onload = function() {
+    
     var faqs = $("faqs");
     var linkElements = faqs.getElementsByTagName("a");
-       
+    console.log(2);
+
     for (var i = 0; i < linkElements.length; i++ ) {
+        console.log(3);
+
     	linkElements[i].onclick = toggle;
     }
     linkElements[0].focus();       
---
+++ faqs/js/faqs.js
@@ -0,0 +1,51 @@
+// "use strict";
+// var $ = function(id) { return document.getElementById(id); };
+
+// the event handler for the click event of each h2 element
+/* var toggle = function() {
+    // console.log(this);
+    var link = this;                    // clicked h2 tag     
+    var h2 = link.parentNode;
+    var div = h2.next();  // h2 tag's sibling div tag
+
+    // toggle plus and minus image in h2 elements by adding or removing a class
+    if (h2.hasAttribute("class")) { 
+        h2.removeAttribute("class");	
+    } else { 
+        h2.className = "minus";
+    }
+
+    // toggle div visibility by adding or removing a class
+    if (div.hasAttribute("class")) { 
+        div.removeAttribute("class");
+    } else { 
+        div.className = "open"; 
+    } 
+};
+
+window.onload = function() {
+    
+    var faqs = $("faqs");
+    var linkElements = faqs.getElementsByTagName("a");
+    console.log(2);
+
+    for (var i = 0; i < linkElements.length; i++ ) {
+        console.log(3);
+
+    	linkElements[i].onclick = toggle;
+    }
+    linkElements[0].focus();       
+}; */
+
+$(document).ready(function(){
+    $("#faqs h2").click(function(evt){
+        $(this).toggleClass("minus");
+        if ($(this).attr("class") !== "minus"){
+            $(this).next().hide();
+        } else {
+            $(this).next().show();
+        }
+        evt.preventDefault();
+    });
+    //$("#faqs").find("a:first").focus();
+});
