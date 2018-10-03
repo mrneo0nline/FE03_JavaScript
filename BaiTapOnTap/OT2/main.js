ee00ef6e52f482af802194b49a60c94d50dd8de8
mrneo0nline <39259021+mrneo0nline@users.noreply.github.com>
2018-10-04 01:01:27 +0700

    Completed!

--- BaiTapOnTap/OT2/main.js
+++ BaiTapOnTap/OT2/main.js
@@ -1,17 +1,10 @@
 $(document).ready(function () {
 	"use strict";
-	// load tableArray onload
+	// load tableArray onload of page
 	var todoList = ['Hoc CSS', 'Hoc React JS', 'Hoc Front End', 'On Tap'];
-	/* 	var tblHTML = '';
-		var editedTbl = '';
-		var delTbl = '';
-		var addTbl = ''; */
-	var pText;
 
-	// hide Input todolist onload
-
-	/* 	$(".edit_OK, .edit_cancel").hide();
-		$(".edtTodos").hide(); */
+	showData(todoList);
+	$("#formAdd").hide();
 
 	// Function to show data base on array inputted
 	function showData(arrTbl) {
@@ -19,90 +12,100 @@ $(document).ready(function () {
 		$.each(arrTbl, function (i) {
 			tblHTML +=
 				'<tr>' +
-					'<td>' + (i + 1) + '</td>' +
-					'<td><input type=\'checkbox\'></td>' +
-					'<td>' +
-						'<p>' + arrTbl[i] + '</p>' +
-						'<input type=\"text\" class=\"edtTodos\">' +
-						'<input type=\"button\" class=\"btn btn-light ml-2 border-success edit_OK\" value=\"OK\">' +
-						'<input type=\"button\" class=\"btn btn-light ml-2 border-danger edit_cancel\" value=\"Cancel\">' +
-					'</td>' +
-					'<td>' +
-						'<input type=\"button\" class=\"btn btn-light ml-2 border-success btn_edit\" value=\"Edit\">' +
-						'<input id=\"' + i + '\" type=\"button\" class=\"btn btn-light ml-2 border-danger btn_del\" value=\"Delete\">' +
-					'</td>' +
+				'<td>' + (i + 1) + '</td>' +
+				'<td><input type=\'checkbox\'></td>' +
+				'<td>' +
+				'<p>' + arrTbl[i] + '</p>' +
+				'<input type=\"text\" class=\"edtTodos\">' +
+				'</td>' +
+				'<td>' +
+				'<input type=\"button\" class=\"btn btn-light ml-2 border-success btn_edit\" value=\"Edit\">' +
+				'<input type=\"button\" class=\"btn btn-light ml-2 border-danger btn_del\" value=\"Delete\">' +
+				'<input type=\"button\" class=\"btn btn-success ml-2 border-light btn-edit-group edit_OK\" value=\"Confirm\">' +
+				'<input type=\"button\" class=\"btn btn-danger ml-2 border-light btn-edit-group edit_cancel\" value=\"Cancel\">' +
+				'</td>' +
 				'</tr>';
 		});
 		$("tbody").html(tblHTML);
 		$(".edit_OK, .edit_cancel").hide();
 		$(".edtTodos").hide();
 	};
-	showData(todoList);
-	$("#formAdd").hide();
 
-	// hide Add button, and show Input todolist form
+	// hide Add button when input field appears
 	$("#btn_add").click(function () {
 		$("#formAdd").show();
-		$(".taskName").val('');
+		$(".taskName").val('').focus();
 		$("#addBtn").hide();
 	});
 
-	// show Add button when pressing Cancel, and hide AddForm
+	// show Add button and hide input field after pressed Cancel button
 	$("#cancelled").click(function () {
 		$(".taskName").val('');
 		$("#formAdd").hide();
 		$("#addBtn").show();
 	});
 
-	//ADD_Confirm button
+	//Add To-do list
 	$(".btn_add").click(function () {
-		todoList.push($('#formAdd input[type=text]').val());
-		showData(todoList);
-		$(".taskName").focus();
-		$(".taskName").val("");
+
+		// Add new to-do list
+		var isBlank = $('.taskName').val();
+
+		if (isBlank === '') {
+			alert('You must input name for the task!');
+			$('#formAdd input[type=text]').focus();
+		} else {
+			todoList.push($('#formAdd input[type=text]').val());
+			showData(todoList);
+			$(".taskName").focus();
+			$(".taskName").val("");
+		};
+	});
+
+	//Edit To-do list
+	$('table').on('click', '.btn_edit', function () {
+
+		//Edit button
+		var pText = $(this).parent().prev().children('p').text();
+
+		$(this).parent().prev().children('p').hide();
+		$(this).parent().children(".btn_edit, .btn_del").hide();
+		$(this).parent().children('.btn-edit-group').show();
+		$(this).parent().prev().children("input[type=text]").show().val(pText).focus();
+
+		//Cancel button
+		$('table').on('click', '.edit_cancel', function () {
+			$(this).parent().prev().children('p').show();
+			$(this).parent().children(".btn_edit, .btn_del").show();
+			$(this).parent().children('.btn-edit-group').hide();
+			$(this).parent().prev().children("input[type=text]").hide();
+		});
+
+		//OK button
+		$('table').on('click', '.edit_OK', function () {
+			var index = $(this).closest('tr').children(':first-child').text() - 1;
+			var updatedInfo = $(this).parent().prev().children("input[type=text]");
+
+			if ( updatedInfo.val() == '') {
+				alert('You must not input a blanked task name!');
+				updatedInfo.focus();
+			} else {
+			todoList.splice(index, 1, updatedInfo.val());
+			showData(todoList);
+			};
+		});
 	});
 
 	//DELETE button
-	$('table').on('click', '.btn_del', function(){
-		var index = this.id.substring(1);
+	$('table').on('click', '.btn_del', function () {
+		var index = $(this).closest('tr').children(':first-child').text() - 1;
+		var confirmation = confirm('Are you SURE to DELETE IT ?');
+		if ( !confirmation ) {
+			showData(todoList);
+		} else {
 		todoList.splice(index, 1);
-		// todoList.splice($(this).closest("tr").children("td:first-child").text()-1, 1);
 		showData(todoList);
-	});
-/* 	$(".btn_del").click(function () {
-		var index = this.id.substring(1);
-		todoList.splice(index, 1);
-		// todoList.splice($(this).closest("tr").children("td:first-child").text()-1, 1);
-		showData(todoList);
-	}); */
+		}
+	}); // End delete function
 
-	//Edit button
-	$(".btn_edit").click(function () {
-		pText = $(this).parent().prev().children("p").text();
-		$(this).parent().prev().children().show();
-		$(this).parent().prev().children("input[type=text]").val(pText);
-		$(this).parent().prev().children("input[type=text]").focus();
-		$(this).parent().prev().children("p").hide();
-		$(this).parent().children(".btn_edit, .btn_del").hide();
-	});
-
-	//OK button in Edit form
-	$(".edit_OK").click(function () {
-		todoList.splice(parseInt($(this).closest("tr").children("td:first-child").text()) - 1, 1, $(this).parent().children(".edtTodos").val());
-		$("table tbody").empty();
-		$.each(todoList, function (i) {
-			editedTbl += '<tr><td>' + (i + 1) + '</td><td><input type=\'checkbox\'></td><td><p>' + todoList[i] + '</p><input type=\"text\" class=\"edtTodos\"><input type=\"button\" class=\"btn btn-light ml-2 border-success edit_OK\" value=\"OK\"><input type=\"button\" class=\"btn btn-light ml-2 border-danger edit_cancel\" value=\"Cancel\"></td><td><input type=\"button\" class=\"btn btn-light ml-2 border-success btn_edit\" value=\"Edit\"><input type=\"button\" class=\"btn btn-light ml-2 border-danger btn_del\" value=\"Delete\"></td></tr>';
-		});
-		$("table tbody").append(editedTbl);
-		$(".edit_OK, .edit_cancel").hide();
-		$(".edtTodos").hide();
-	});
-
-	//Cancel button in Edit form
-	$(".edit_cancel").click(function () {
-		$(this).parent("td").next().children().show();
-		$(this).parent().children("p").show();
-		$(this).parent().children(".edtTodos, .edit_OK, .edit_cancel").hide();
-	});
-
-});
+}); // End document ready functions
